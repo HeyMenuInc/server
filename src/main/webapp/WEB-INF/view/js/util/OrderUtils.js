fbee82270523e9417e9b5d0a1109c3b50ff1a1db
charliezhang <zero891109@gmail.com>
2014-04-29 22:18:16 -0700

    some more utility JS

--- src/main/webapp/WEB-INF/view/js/util/OrderUtils.js
+++ src/main/webapp/WEB-INF/view/js/util/OrderUtils.js
@@ -2,26 +2,40 @@
 /*global define */
 
 define(function (require, exports, module) {
-  var $ = require('jquery');
+    var $ = require('jquery');
 
-  exports.createOrder = function(tableId, orderName) {
-  // create order
-    $.ajax({
-        contentType: 'application/json',
-        dataType: 'json',
-        url:'/api/orders',
-        type: 'POST',
-        data: JSON.stringify({
-            tableId: 1,
-            dishes: [{id:1},{id:2}], name: "order1"
-        }),
-        success: function(data){
-            console.log("create order succeeded");
-        },
-        error: function(){
-            console.log("create order failed");
-        }
-    });
-  }
+    exports.clearTable = function(tableId) {
+        $.ajax({
+            url: '/api/tables/1/clear',
+            type: 'PUT'
+        });
+    }
+
+    exports.occupyTable = function(tableId) {
+        $.ajax({
+            url: '/api/tables/1/occupy?customerNumber=2',
+            type: 'PUT'
+        });
+    }
+
+    // create order
+    exports.createOrder = function(tableId, orderName) {
+        $.ajax({
+            contentType: 'application/json',
+            dataType: 'json',
+            url:'/api/orders',
+            type: 'POST',
+            data: JSON.stringify({
+                tableId: 1,
+                dishes: [{id:1},{id:2}], name: "order1"
+            }),
+            success: function(data){
+                console.log("create order succeeded");
+            },
+            error: function(){
+                console.log("create order failed");
+            }
+        });
+    }
 }
 
